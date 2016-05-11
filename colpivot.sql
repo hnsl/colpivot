9f1f1db6d348cb78e7e921133a487ac569f89b4d
Antti Linno <antti@begin.ee>
2016-05-11 15:42:13 +0300

    When column value is null, it cannot be joined with equality

--- colpivot.sql
+++ colpivot.sql
@@ -74,6 +74,7 @@ create or replace function colpivot(
             query := query || '_key.' || quote_ident(col) || ' ';
             i := i + 1;
         end loop;
+        -- empty resultset, bail out
         if n_clsc_cols is null then
             execute ('drop table ' || in_table);
             return false;
@@ -90,6 +91,7 @@ create or replace function colpivot(
             ali := '_clsc_' || j::text;
             query := query || '(' || replace(value_e, '#', ali) || ')' || ' as ' || quote_ident(col) || ' ';
         end loop;
+        -- from part
         query := query || ' from (select distinct ';
         i := 0;
         foreach col in array key_cols loop
@@ -107,8 +109,10 @@ create or replace function colpivot(
             foreach col in array key_cols loop
                 if i > 0 then
                     on_e := on_e || ' and ';
+                    on_e := on_e || '(' || ali || '.' || quote_ident(col) || ' = _key.' || quote_ident(col) || ' OR (' || ali || '.' || quote_ident(col) || ' IS NULL AND _key.' || quote_ident(col) || ' IS NULL)) ';
+                else
+                    on_e := on_e || ali || '.' || quote_ident(col) || ' = _key.' || quote_ident(col) || ' ';
                 end if;
-                on_e := on_e || ali || '.' || quote_ident(col) || ' = _key.' || quote_ident(col) || ' ';
                 i := i + 1;
             end loop;
             for k in 1..n_class_cols loop
